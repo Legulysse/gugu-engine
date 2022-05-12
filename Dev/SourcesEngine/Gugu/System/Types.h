e2757c5e1d7b59a69a4d2bed0805c9bf8b940425
Legulysse <legulysse@hotmail.com>
2022-05-12 11:05:15 +0200

    Use cstdint base types instead of old typedefs

--- Dev/SourcesEngine/Gugu/System/Types.h
+++ Dev/SourcesEngine/Gugu/System/Types.h
@@ -1,29 +1,23 @@
 #pragma once
 
 ////////////////////////////////////////////////////////////////
+// Includes
+
+#include <cstdint>
+
+////////////////////////////////////////////////////////////////
 // File Declarations
 
 namespace gugu {
     
-// 8 bits integer types
-typedef signed   char int8;
-typedef unsigned char uint8;
+using int8 = std::int8_t;
+using int16 = std::int16_t;
+using int32 = std::int32_t;
+using int64 = std::int64_t;
 
-// 16 bits integer types
-typedef signed   short int16;
-typedef unsigned short uint16;
+using uint8 = std::uint8_t;
+using uint16 = std::uint16_t;
+using uint32 = std::uint32_t;
+using uint64 = std::uint64_t;
 
-// 32 bits integer types
-typedef signed   int int32;
-typedef unsigned int uint32;
-
-// 64 bits integer types
-#if defined(_MSC_VER)
-    typedef signed   __int64 int64;
-    typedef unsigned __int64 uint64;
-#else
-    typedef signed   long long int64;
-    typedef unsigned long long uint64;
-#endif
-    
 }   // namespace gugu

49bc988dd85a807cc1104bc2a923f92c3e3c3552
Legulysse <legulysse@hotmail.com>
2022-07-29 23:18:11 +0200

    Fix potential crash with debug bounds, allow the editor WidgetRenderer to display bounds if needed

--- Dev/SourcesEditor/Gugu/Editor/Widget/WidgetRenderer.cpp
+++ Dev/SourcesEditor/Gugu/Editor/Widget/WidgetRenderer.cpp
@@ -18,9 +18,15 @@ namespace gugu {
 
 void WidgetRenderer::RenderWidget(sf::RenderTexture* renderTexture, Element* root)
 {
+    FrameInfos frameInfos;
+    frameInfos.defaultBoundsShape.setOutlineThickness(-1.f);
+    frameInfos.defaultBoundsShape.setOutlineColor(sf::Color(255, 0, 255, 200));
+    frameInfos.defaultBoundsShape.setFillColor(sf::Color::Transparent);
+
     RenderPass renderPass;
     renderPass.pass = GUGU_RENDERPASS_DEFAULT;
     renderPass.target = renderTexture;
+    renderPass.frameInfos = &frameInfos;
 
     RenderElementHierarchy(renderPass, root, nullptr);
 }

58aea736dfc7e7fe6846bd21891de9050511a838
Legulysse <legulysse@hotmail.com>
2022-08-30 17:00:31 +0200

    Add macros for std::bind placeholders

--- Dev/SourcesEngine/Gugu/Core/Callback.h
+++ Dev/SourcesEngine/Gugu/Core/Callback.h
@@ -6,6 +6,15 @@
 #include <functional>
 
 ////////////////////////////////////////////////////////////////
+// Macros
+
+#define STD_BIND  (FUNCTION, OBJECT) std::bind(FUNCTION, OBJECT)
+#define STD_BIND_1(FUNCTION, OBJECT) std::bind(FUNCTION, OBJECT, std::placeholders::_1)
+#define STD_BIND_2(FUNCTION, OBJECT) std::bind(FUNCTION, OBJECT, std::placeholders::_1, std::placeholders::_2)
+#define STD_BIND_3(FUNCTION, OBJECT) std::bind(FUNCTION, OBJECT, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3)
+#define STD_BIND_4(FUNCTION, OBJECT) std::bind(FUNCTION, OBJECT, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3, std::placeholders::_4)
+
+////////////////////////////////////////////////////////////////
 // File Declarations
 
 namespace gugu {

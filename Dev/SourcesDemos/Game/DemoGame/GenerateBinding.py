a2ea74cf00bab45244bc04169d02b0fbb9d97eac
Legulysse <legulysse@hotmail.com>
2023-01-27 20:30:37 +0100

    Rename DatasheetBindingTool into DataBindingTool, update demos setup

--- Dev/SourcesDemos/Game/DemoGame/GenerateBinding.py
+++ Dev/SourcesDemos/Game/DemoGame/GenerateBinding.py
@@ -1,14 +1,14 @@
 import sys
 
 sys.path.append('../../../SourcesEngine/Gugu')
-from DatasheetBindingTool import *
+from DataBindingTool import *
 
 
 # Generates the C++ binding from the xml binding.
 # > _pathBindingXml : the path to the source xml definition.
 # > _pathBindingCpp : the destination folder for the generated C++ files ('.h' and '.cpp' files).
-# > _generatedFileName : the base name for generated C++ files (If empty, defaults to DatasheetBinding).
-GenerateBindingCpp('../../../../Version/DemoGame/Editor/DatasheetBinding.xml', '', '')
+# > _generatedFileName : the base name for generated C++ files (If empty, defaults to DataBinding).
+GenerateBindingCpp('../../../../Version/DemoGame/Editor/DataBinding.xml', '', '')
 
 
 input('Press Enter to close.')
--- Dev/SourcesDemos/Systems/DemoDatasheet/GenerateBinding.py
+++ Dev/SourcesDemos/Systems/DemoDatasheet/GenerateBinding.py
@@ -1,14 +1,14 @@
 import sys
 
 sys.path.append('../../../SourcesEngine/Gugu')
-from DatasheetBindingTool import *
+from DataBindingTool import *
 
 
 # Generates the C++ binding from the xml binding.
 # > _pathBindingXml : the path to the source xml definition.
 # > _pathBindingCpp : the destination folder for the generated C++ files ('.h' and '.cpp' files).
-# > _generatedFileName : the base name for generated C++ files (If empty, defaults to DatasheetBinding).
-GenerateBindingCpp('../../../../Version/DemoSystems/Editor/DatasheetBinding.xml', '', 'DatasheetBinding_Generated')
+# > _generatedFileName : the base name for generated C++ files (If empty, defaults to DataBinding).
+GenerateBindingCpp('../../../../Version/DemoSystems/Editor/DataBinding.xml', '', 'DataBinding_Generated')
 
 
 input('Press Enter to close.')

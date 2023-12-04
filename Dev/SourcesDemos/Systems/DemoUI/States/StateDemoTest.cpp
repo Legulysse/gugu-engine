a718715403b602196aede08e3e2e203a40f24757
Legulysse <legulysse@hotmail.com>
2023-12-04 23:51:35 +0100

    Add slider widget in demo

--- Dev/SourcesDemos/Systems/DemoUI/States/StateDemoTest.cpp
+++ Dev/SourcesDemos/Systems/DemoUI/States/StateDemoTest.cpp
@@ -20,6 +20,9 @@
 #include "Gugu/Element/2D/ElementSprite.h"
 #include "Gugu/Element/UI/ElementEditableText.h"
 #include "Gugu/Element/UI/ElementButton.h"
+#include "Gugu/Element/UI/ElementCheckbox.h"
+#include "Gugu/Element/UI/ElementSlider.h"
+#include "Gugu/Element/UI/ElementLayoutGroup.h"
 #include "Gugu/Element/UI/ElementList.h"
 #include "Gugu/Element/UI/ElementListItem.h"
 
@@ -42,33 +45,62 @@ void StateDemoTest::Init()
 {
     RegisterEventHandler(GetGameWindow());
 
-    //Root
+    // Root
     m_root = GetGameWindow()->GetUINode()->AddChild<Element>();
     m_root->SetUnifiedSize(UDim2::SIZE_FULL);
 
-    //Background
+    // Background
     ElementSprite* pTile = m_root->AddChild<ElementSprite>();
     pTile->SetTexture("Background.jpg");
     pTile->SetRepeatTexture(true);
     pTile->SetUnifiedSize(UDim2::SIZE_FULL);
 
-    //Test EditLine
-    ElementEditableText* pEditLine = m_root->AddChild<ElementEditableText>();
-    //pEditLine->SetResizeRule(TextResizeRule::FitScale);
-    pEditLine->SetText("Edit Me !");
-    pEditLine->SetMultiline(false);
-    pEditLine->SetEditable(true);
-    pEditLine->SetPosition(10.f, 10.f);
-    //pEditLine->SetSize(300.f, 200.f);
-    pEditLine->SetDebugBoundsVisible(true);
+    // Vertical layout
+    ElementLayoutGroup* layout = m_root->AddChild<ElementLayoutGroup>();
+    layout->SetPosition(10.f, 10.f);
+    layout->SetItemSpacing(4.f);
+    layout->SetMainDirection(ELayoutDirection::TopToBottom);
 
-    //List
+    // Button
+    ElementButton* button = new ElementButton;
+    button->LoadFromWidget("Button_01.widget.xml");
+    button->SetText("Press Me");
+    button->SetDebugBoundsVisible(true);
+    layout->AddItem(button);
+
+    // Checkbox
+    ElementCheckbox* checkbox = new ElementCheckbox;
+    checkbox->LoadFromWidget("Checkbox_01.widget.xml");
+    checkbox->SetText("Toggle Me");
+    checkbox->SetChecked(true);
+    checkbox->SetDebugBoundsVisible(true);
+    layout->AddItem(checkbox);
+
+    // Slider
+    ElementSlider* slider = new ElementSlider;
+    slider->LoadFromWidget("Slider_01.widget.xml");
+    slider->SetValueLimits(-100, 100);
+    slider->SetValue(50);
+    slider->SetDebugBoundsVisible(true);
+    layout->AddItem(slider);
+
+    ElementText* sliderText = slider->AddChild<ElementText>();
+    sliderText->SetUnifiedPosition(UDim2::POSITION_TOP_RIGHT);
+    sliderText->SetText(ToString(slider->GetValue()));
+
+    slider->SetOnValueChanged([=]()
+    {
+        sliderText->SetText(ToString(slider->GetValue()));
+    });
+
+    // List
     ElementList* pList = m_root->AddChild<ElementList>();
     pList->SetPosition(40.f, 250.f);
     pList->SetSize(200.f, 200.f);
     //pList->SetScaleY(0.7f);
     pList->Rotate(22.5f);
     pList->SetImageSet("ScrollBar_01.imageset.xml");
+    pList->SetDebugBoundsVisible(true);
 
     for (int i=0; i<51; ++i)
     {
@@ -82,14 +114,24 @@ void StateDemoTest::Init()
         pList->AddItem(pItem);
     }
 
-    //Test Multilined text
+    // Editable single line text
+    ElementEditableText* pEditLine = m_root->AddChild<ElementEditableText>();
+    //pEditLine->SetResizeRule(TextResizeRule::FitScale);
+    pEditLine->SetText("Single Line - Edit Me");
+    pEditLine->SetMultiline(false);
+    pEditLine->SetEditable(true);
+    pEditLine->SetPosition(300.f, 10.f);
+    //pEditLine->SetSize(300.f, 200.f);
+    pEditLine->SetDebugBoundsVisible(true);
+
+    // Editable multiline text
     ElementEditableText* pText = m_root->AddChild<ElementEditableText>();
     pText->SetResizeRule(ETextResizeRule::FitHeight);
     pText->SetSize(600.f, 800.f);
     pText->SetMultiline(true);
     pText->SetEditable(true);
-    pText->SetText("Graeci recteque\n\n molestiae   vel ex, no est dico dissentiet, an dolor legere habemus eos. Regione perfecto delicatissimi ei vis, eu commodo pertinacia reprimique per. Dolore dissentiunt quo eu. Mea harum probatus efficiendi cu. Animal aliquid id qui, tibique probatus at duo, ne mea summo laudem definitionem. Amet stet dicta ei per. Ius errem luptatum instructior cu, eu integre facilisis adipiscing duo, ex ubique eripuit debitis qui.\n Ut sit everti electram theophrastus, sumo expetendis pri ad, has officiis pertinax ex. Quod fabellas cu vim, ei qui impedit mnesarchum, eum veniam admodum at. Est elit oratio eu, vim ferri possim denique an. Eu clita dolores sed, vis mnesarchum percipitur dissentiet ex, sea rebum scripta deterruisset te. No praesent imperdiet rationibus his.");
-    pText->SetPosition(300.f, 0.f);
+    pText->SetText("Multiple Lines - Edit Me\n\nGraeci recteque molestiae vel ex, no est dico dissentiet, an dolor legere habemus eos. Regione perfecto delicatissimi ei vis, eu commodo pertinacia reprimique per. Dolore dissentiunt quo eu. Mea harum probatus efficiendi cu. Animal aliquid id qui, tibique probatus at duo, ne mea summo laudem definitionem. Amet stet dicta ei per. Ius errem luptatum instructior cu, eu integre facilisis adipiscing duo, ex ubique eripuit debitis qui.\n Ut sit everti electram theophrastus, sumo expetendis pri ad, has officiis pertinax ex. Quod fabellas cu vim, ei qui impedit mnesarchum, eum veniam admodum at. Est elit oratio eu, vim ferri possim denique an. Eu clita dolores sed, vis mnesarchum percipitur dissentiet ex, sea rebum scripta deterruisset te. No praesent imperdiet rationibus his.");
+    pText->SetPosition(300.f, 50.f);
     pText->SetDebugBoundsVisible(true);
 }
 

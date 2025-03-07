dba86486ea2dbadcdb9c39c8aeb811b8ed9dbb78
Legulysse <legulysse@hotmail.com>
2025-03-07 21:32:13 +0100

    Handle symmetric horizontal trimming, add result notes file

--- Dev/SourcesEditor/Gugu/Editor/Modal/ImportImageSetDialog.cpp
+++ Dev/SourcesEditor/Gugu/Editor/Modal/ImportImageSetDialog.cpp
@@ -13,6 +13,7 @@
 #include "Gugu/System/String.h"
 #include "Gugu/Math/MathUtility.h"
 #include "Gugu/External/ImGuiUtility.h"
+#include "Gugu/Debug/Logger.h"
 
 #include <SFML/Graphics/Image.hpp>
 #include <SFML/Graphics/Texture.hpp>
@@ -45,6 +46,7 @@ void ImportImageSetDialog::UpdateModalImpl(const DeltaTime& dt)
     // - Target directory will be outside of the source directory.
     // - We want one row per animation in the final texture.
     // - We want all individual frames to have the same size.
+    // - We want to trim empty spacings between frames, but with a symmetrical rule (keep all frames centered).
     // - We want a final texture size aligned on a power of 2 (not necessarily squared).
     // - ImageMagick has been setup in a predefined directory.
     // - We always apply an unsharp pass after the resize.
@@ -81,13 +83,12 @@ void ImportImageSetDialog::UpdateModalImpl(const DeltaTime& dt)
 
         std::string resizeFilter = filters[m_filterIndex];
 
-        std::string targetResizeDirectory = CombinePaths(m_targetDirectory, resizeFilter);
+        std::string targetResizeDirectory = m_targetDirectory;  // CombinePaths(m_targetDirectory, resizeFilter);
         std::string targetPackingDirectory = targetResizeDirectory;
         FileInfo targetPackingFile = FileInfo::FromString_utf8(CombinePaths(targetPackingDirectory, m_targetTextureName));
 
         // Prepare Image data.
         std::vector<sf::Image*> sourceImages;
-        sf::Image* targetImage = nullptr;
         std::vector<int> frameCountPerAnimation;
 
         // Clear target directory.
@@ -139,33 +140,62 @@ void ImportImageSetDialog::UpdateModalImpl(const DeltaTime& dt)
         // Parse packing source files.
         if (!sourceImages.empty())
         {
-            Vector2u maxImageSize = Vector2::Zero_u;
+            EnsureDirectoryExists(targetPackingFile.GetDirectoryPath_utf8());
+
+            sf::Image* targetImage = nullptr;
+
+            // Compute minimum width with trim for source images.
+            // This is meant to handle frames with varying size.
+            // All frames greater than this size can be trimmed.
+            Vector2i maxFrameImageSize = Vector2::Zero_i;
+            int maxTrimmedWidth = 0;
+
             for (const auto& sourceImage : sourceImages)
             {
-                maxImageSize.x = Max(maxImageSize.x, sourceImage->getSize().x);
-                maxImageSize.y = Max(maxImageSize.y, sourceImage->getSize().y);
+                int width = sourceImage->getSize().x;
+                int height = sourceImage->getSize().y;
+                bool canTrimHorizontally = true;
+                int horizontalTrim = 0;
+
+                while (canTrimHorizontally && horizontalTrim * 2 < width)
+                {
+                    for (int y = 0; y < height; ++y)
+                    {
+                        canTrimHorizontally &= sourceImage->getPixel(horizontalTrim, y) == sf::Color::Transparent;
+                        canTrimHorizontally &= sourceImage->getPixel(width - 1 - horizontalTrim, y) == sf::Color::Transparent;
+                    }
+
+                    if (canTrimHorizontally)
+                    {
+                        ++horizontalTrim;
+                    }
+                }
+
+                int trimmedWidth = width - horizontalTrim * 2;
+                maxTrimmedWidth = Max(maxTrimmedWidth, trimmedWidth);
+
+                maxFrameImageSize.x = Max(maxFrameImageSize.x, maxTrimmedWidth);
+                maxFrameImageSize.y = Max(maxFrameImageSize.y, height);
             }
 
+            // Compute target size (align to the nearest power of 2).
             int maxFrameCount = 0;
             for (const auto& frameCount : frameCountPerAnimation)
             {
                 maxFrameCount = Max(frameCount, maxFrameCount);
             }
 
-            EnsureDirectoryExists(targetPackingFile.GetDirectoryPath_utf8());
-
-            // Compute target size (align to the nearest power of 2).
-            int width = maxFrameCount * maxImageSize.x;
-            int height = (int)frameCountPerAnimation.size() * maxImageSize.y;
-            Vector2u minimumImageSize = Vector2u(width, height);
+            int width = maxFrameCount * maxFrameImageSize.x;
+            int height = (int)frameCountPerAnimation.size() * maxFrameImageSize.y;
+            Vector2u minTargetImageSize = Vector2u(width, height);
             Vector2u targetImageSize = Vector2u(2, 2);
 
-            while (targetImageSize.x < maxTextureSize.x && targetImageSize.x < minimumImageSize.x)
+            while (targetImageSize.x < maxTextureSize.x && targetImageSize.x < minTargetImageSize.x)
             {
                 targetImageSize.x *= 2;
             }
 
-            while (targetImageSize.y < maxTextureSize.y && targetImageSize.y < minimumImageSize.y)
+            while (targetImageSize.y < maxTextureSize.y && targetImageSize.y < minTargetImageSize.y)
             {
                 targetImageSize.y *= 2;
             }
@@ -179,7 +209,26 @@ void ImportImageSetDialog::UpdateModalImpl(const DeltaTime& dt)
             int frameIndex = 0;
             for (size_t i = 0; i < sourceImages.size(); ++i)
             {
-                targetImage->copy(*sourceImages[i], frameIndex * maxImageSize.x, animationIndex * maxImageSize.y);
+                // If offset < 0 then the source is larger than the target destination.
+                // If offset > 0 then the source is smaller than the target destination.
+                int frameOffsetX = 0;
+                if ((int)sourceImages[i]->getSize().x != maxFrameImageSize.x)
+                    frameOffsetX = RoundNearestInt(RoundCeil((maxFrameImageSize.x - (int)sourceImages[i]->getSize().x) * 0.5f));
+
+                int frameOffsetY = 0;
+                if ((int)sourceImages[i]->getSize().y != maxFrameImageSize.y)
+                    frameOffsetY = RoundNearestInt(RoundCeil((maxFrameImageSize.y - (int)sourceImages[i]->getSize().y) * 0.5f));
+
+                int frameDestOffsetX = Max<int>(0, frameOffsetX);
+                int frameDestOffsetY = Max<int>(0, frameOffsetY);
+                Vector2u frameRectOffset = Vector2u(Absolute(Min<int>(0, frameOffsetX)), Absolute(Min<int>(0, frameOffsetY)));
+                Vector2u frameRectSize = sourceImages[i]->getSize() - Vector2u(frameRectOffset.x * 2, frameRectOffset.y * 2);
+                sf::IntRect frameRect = sf::IntRect(Vector2i(frameRectOffset), Vector2i(frameRectSize));
+
+                targetImage->copy(*sourceImages[i],
+                    frameIndex * maxFrameImageSize.x + frameDestOffsetX,
+                    animationIndex * maxFrameImageSize.y + frameDestOffsetY,
+                    frameRect);
                 ++frameIndex;
 
                 if (frameIndex >= frameCountPerAnimation[animationIndex])
@@ -195,6 +244,16 @@ void ImportImageSetDialog::UpdateModalImpl(const DeltaTime& dt)
             // Release data.
             ClearStdVector(sourceImages);
             SafeDelete(targetImage);
+
+            // Generate Notes file.
+            FileInfo resultNotesPath = FileInfo::FromString_utf8(StringFormat("{0}.txt", CombinePaths(targetPackingFile.GetDirectoryPath_utf8(), targetPackingFile.GetPrettyName())));
+            
+            RemoveFile(resultNotesPath.GetFileSystemPath());
+            WriteInFileEndline(resultNotesPath.GetFileSystemPath(), StringFormat("Frame Size: {0} x {1}", maxFrameImageSize.x, maxFrameImageSize.y));
+            WriteInFileEndline(resultNotesPath.GetFileSystemPath(), StringFormat("Scale: {0}%", m_resizeScale));
+            WriteInFileEndline(resultNotesPath.GetFileSystemPath(), StringFormat("Filter: {0}", resizeFilter));
+
+            GetLogEngine()->Print(ELog::Info, ELogEngine::Editor, StringFormat("Imported ImageSet Succesful, frame size = {0}", maxFrameImageSize));
         }
     }
 

efb1b1d3bf41c2b6c9cf609177389280015106db
Legulysse <legulysse@hotmail.com>
2025-12-24 14:41:49 +0100

    Add data in LocalizationTables

--- Dev/SourcesEngine/Gugu/Resources/LocalizationTable.cpp
+++ Dev/SourcesEngine/Gugu/Resources/LocalizationTable.cpp
@@ -24,6 +24,75 @@ LocalizationTable::~LocalizationTable()
     Unload();
 }
 
+LocalizationRegisterResult LocalizationTable::TryRegisterEntry(const LocalizationLanguageCode& language, const LocalizationKey& key, const LocalizationTextEntry& entry)
+{
+    if (entry.text.empty())
+    {
+        return LocalizationRegisterResult::Refused_EmptyText;
+    }
+
+    auto itLanguage = m_languageTables.find(language);
+    if (itLanguage == m_languageTables.end())
+    {
+        itLanguage = m_languageTables.insert(itLanguage, std::make_pair(language, LocalizationLanguageTable()));
+    }
+
+    auto itEntry = itLanguage->second.entries.find(key);
+    if (itEntry == itLanguage->second.entries.end())
+    {
+        itEntry = itLanguage->second.entries.insert(itEntry, std::make_pair(key, entry));
+        return LocalizationRegisterResult::Accepted;
+    }
+    else
+    {
+        if (itEntry->second.timestamp > entry.timestamp)
+        {
+            return LocalizationRegisterResult::Refused_Outdated;
+        }
+        else if (itEntry->second.timestamp == entry.timestamp)
+        {
+            if (itEntry->second.text == entry.text)
+            {
+                return LocalizationRegisterResult::Refused_Identical;
+            }
+            else    // if (itEntry->second.text != entry.text)
+            {
+                itEntry->second = entry;
+                return LocalizationRegisterResult::Accepted_IdenticalTimestamp;
+            }
+        }
+        else    // if (itEntry->second.timestamp < entry.timestamp)
+        {
+            if (itEntry->second.text == entry.text)
+            {
+                itEntry->second = entry;
+                return LocalizationRegisterResult::Accepted_IdenticalText;
+            }
+            else    // if (itEntry->second.text != entry.text)
+            {
+                itEntry->second = entry;
+                return LocalizationRegisterResult::Accepted;
+            }
+        }
+    }
+}
+
+const std::string& LocalizationTable::GetText(const LocalizationLanguageCode& language, const LocalizationKey& key) const
+{
+    auto itLanguage = m_languageTables.find(language);
+    if (itLanguage != m_languageTables.end())
+    {
+        auto itEntry = itLanguage->second.entries.find(key);
+        if (itEntry != itLanguage->second.entries.end())
+        {
+            return itEntry->second.text;
+        }
+    }
+
+    static const std::string defaultValue = "LOCA NOT FOUND";
+    return defaultValue;
+}
+
 EResourceType::Type LocalizationTable::GetResourceType() const
 {
     return EResourceType::LocalizationTable;
@@ -31,6 +100,7 @@ EResourceType::Type LocalizationTable::GetResourceType() const
 
 void LocalizationTable::Unload()
 {
+    m_languageTables.clear();
 }
 
 bool LocalizationTable::LoadFromXml(const pugi::xml_document& document)
--- Dev/SourcesEngine/Gugu/Resources/LocalizationTable.h
+++ Dev/SourcesEngine/Gugu/Resources/LocalizationTable.h
@@ -5,17 +5,48 @@
 
 #include "Gugu/Resources/Resource.h"
 
+#include <map>
+
 ////////////////////////////////////////////////////////////////
 // File Declarations
 
 namespace gugu {
-    
+
+using LocalizationLanguageCode = std::string;
+using LocalizationKey = std::string;
+
+struct LocalizationTextEntry
+{
+    std::string text;
+    int64 timestamp;
+};
+
+struct LocalizationLanguageTable
+{
+    std::map<LocalizationKey, LocalizationTextEntry> entries;
+};
+
+enum class LocalizationRegisterResult : uint8
+{
+    Accepted,                       // The new entry has a more recent timestamp than the current entry, or has a new key (new or updated).
+
+    Accepted_IdenticalTimestamp,    // The new entry has the same timestamp as the current entry, but a different text (a review could be necessary).
+    Accepted_IdenticalText,         // The new entry has the same text as the current entry, but with a more recent timestamp (a review could be necessary).
+
+    Refused_Identical,              // The new entry has the same text and timestamp as the new entry (loca is up to date).
+    Refused_EmptyText,              // The new entry has an empty text (invalid).
+    Refused_Outdated,               // The new entry is older than the current entry (outdated).
+};
+
 class LocalizationTable : public Resource
 {
 public:
 
     LocalizationTable();
     virtual ~LocalizationTable();
+
+    LocalizationRegisterResult TryRegisterEntry(const LocalizationLanguageCode& language, const LocalizationKey& key, const LocalizationTextEntry& entry);
+    const std::string& GetText(const LocalizationLanguageCode& language, const LocalizationKey& key) const;
 
     virtual EResourceType::Type GetResourceType() const override;
 
@@ -27,6 +58,7 @@ protected:
 
 protected:
 
+    std::map<LocalizationLanguageCode, LocalizationLanguageTable> m_languageTables;
 };
 
 }   // namespace gugu
